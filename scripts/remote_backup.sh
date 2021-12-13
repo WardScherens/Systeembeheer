87e530f6e0763bba4f012a6967994371f52e1363
WardScherens <ward.scherens@gmail.com>
2021-12-13 12:29:36 +0100

    na lange tijd

--- scripts/remote_backup.sh
+++ scripts/remote_backup.sh
@@ -1,4 +1,1 @@
-#!/usr/bin/expect -f
-	# exp_internal 1
-	set timeout -1
-		spawn rsync -av -e "ssh -p 22345" /etc "r0722535@leia.uclllabs.be:/home/LDAP/r0722535/backup/#
+rsync -a -e 'ssh -p 22345' /etc* r0722535@leia.uclllabs.be:/home/LDAP/r0722535/backup
